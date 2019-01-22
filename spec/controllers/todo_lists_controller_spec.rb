e9654dd67c86455fcb351d0fe60474f5f8dcb8ee
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-22 16:50:38 -0200

    Adds controller test

--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -2,105 +2,97 @@ require 'rails_helper'
 
 RSpec.describe TodoListsController, type: :controller do
 
-  # # This should return the minimal set of attributes required to create a valid
-  # # TodoList. As you add validations to TodoList, be sure to
-  # # adjust the attributes here as well.
-  # let(:valid_attributes) {
-  #   skip("Add a hash of attributes valid for your model")
-  # }
+  let(:valid_session) { {} }
 
-  # let(:invalid_attributes) {
-  #   skip("Add a hash of attributes invalid for your model")
-  # }
+  describe "GET #index" do
+    it "returns a success response" do
+      get :index, params: {}, session: valid_session
 
-  # # This should return the minimal set of values that should be in the session
-  # # in order to pass any filters (e.g. authentication) defined in
-  # # TodoListsController. Be sure to keep this updated too.
-  # let(:valid_session) { {} }
+      expect(response).to be_successful
+    end
+  end
 
-  # describe "GET #index" do
-  #   it "returns a success response" do
-  #     todo_list = TodoList.create! valid_attributes
-  #     get :index, params: {}, session: valid_session
-  #     expect(response).to be_successful
-  #   end
-  # end
+  describe "GET #show" do
+    let(:todo_list) { create(:todo_list) }
 
-  # describe "GET #show" do
-  #   it "returns a success response" do
-  #     todo_list = TodoList.create! valid_attributes
-  #     get :show, params: {id: todo_list.to_param}, session: valid_session
-  #     expect(response).to be_successful
-  #   end
-  # end
+    it "returns a success response" do
+      get :show, params: { id: todo_list.to_param }
+      expect(response).to be_successful
+    end
+  end
 
-  # describe "POST #create" do
-  #   context "with valid params" do
-  #     it "creates a new TodoList" do
-  #       expect {
-  #         post :create, params: {todo_list: valid_attributes}, session: valid_session
-  #       }.to change(TodoList, :count).by(1)
-  #     end
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new TodoList" do
+        expect {
+          post :create, params: {
+                          todo_list: { title: 'Test', description: 'Test' }
+                        }
+        }.to change(TodoList, :count).by(1)
+      end
 
-  #     it "renders a JSON response with the new todo_list" do
+      it "renders a JSON response with the new todo_list" do
 
-  #       post :create, params: {todo_list: valid_attributes}, session: valid_session
-  #       expect(response).to have_http_status(:created)
-  #       expect(response.content_type).to eq('application/json')
-  #       expect(response.location).to eq(todo_list_url(TodoList.last))
-  #     end
-  #   end
+        post :create, params: {
+                                todo_list: { title: 'Test', description: 'Test'}
+                              }
 
-  #   context "with invalid params" do
-  #     it "renders a JSON response with errors for the new todo_list" do
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json')
+        expect(response.location).to eq(todo_list_url(TodoList.last))
+      end
+    end
 
-  #       post :create, params: {todo_list: invalid_attributes}, session: valid_session
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to eq('application/json')
-  #     end
-  #   end
-  # end
+    context "with invalid params" do
+      it "renders a JSON response with errors for the new todo_list" do
+        post :create, params: {
+                                todo_list: { title: nil, description: nil }
+                              }
 
-  # describe "PUT #update" do
-  #   context "with valid params" do
-  #     let(:new_attributes) {
-  #       skip("Add a hash of attributes valid for your model")
-  #     }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
 
-  #     it "updates the requested todo_list" do
-  #       todo_list = TodoList.create! valid_attributes
-  #       put :update, params: {id: todo_list.to_param, todo_list: new_attributes}, session: valid_session
-  #       todo_list.reload
-  #       skip("Add assertions for updated state")
-  #     end
+  describe "PUT #update" do
+    let(:todo_list) { create(:todo_list) }
 
-  #     it "renders a JSON response with the todo_list" do
-  #       todo_list = TodoList.create! valid_attributes
+    context "with valid params" do
+      it "renders a JSON response with the todo_list" do
+        put :update, params: {
+                                id: todo_list.id,
+                                todo_list: { title: 'Test', description: 'Todo'}
+                             }
 
-  #       put :update, params: {id: todo_list.to_param, todo_list: valid_attributes}, session: valid_session
-  #       expect(response).to have_http_status(:ok)
-  #       expect(response.content_type).to eq('application/json')
-  #     end
-  #   end
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
 
-  #   context "with invalid params" do
-  #     it "renders a JSON response with errors for the todo_list" do
-  #       todo_list = TodoList.create! valid_attributes
+    context "with invalid params" do
+      let(:todo_list) { create(:todo_list) }
 
-  #       put :update, params: {id: todo_list.to_param, todo_list: invalid_attributes}, session: valid_session
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to eq('application/json')
-  #     end
-  #   end
-  # end
+      it "renders a JSON response with errors for the todo_list" do
+        put :update, params: {
+                              id: todo_list.id,
+                              todo_list: { title: nil }
+                             }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
 
-  # describe "DELETE #destroy" do
-  #   it "destroys the requested todo_list" do
-  #     todo_list = TodoList.create! valid_attributes
-  #     expect {
-  #       delete :destroy, params: {id: todo_list.to_param}, session: valid_session
-  #     }.to change(TodoList, :count).by(-1)
-  #   end
-  # end
+  describe "DELETE #destroy" do
+    let(:todo_list) { create_list(:todo_list,2) }
+    let(:id1) { todo_list[1].id }
+
+    it "destroys the requested todo_list" do
+      expect {
+        delete :destroy, params: { id: id1 }
+      }.to change(TodoList, :count)
+    end
+  end
 
 end
