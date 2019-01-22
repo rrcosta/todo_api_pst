612ed22f4b1fe2de431d852a3945fe29e9474c34
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-22 15:10:26 -0200

    Adds DatabaseCleaner configurate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,4 +24,16 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  # Database-cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
